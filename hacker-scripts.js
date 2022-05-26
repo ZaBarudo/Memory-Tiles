90b04ca1300f5ac003ee74d6eb0a0732310adbc0
Kevin Christ <smartkevin2003@gmail.com>
2022-05-27 00:28:22 +0530

    Added score in Hacker mode

--- hacker-scripts.js
+++ hacker-scripts.js
@@ -9,6 +9,7 @@ resultText = document.querySelector('.resultText');
 startText = document.querySelector('.startText');
 gameEndSound = document.querySelector('#gameEndSound');
 tilePress = document.querySelector('#tilePress');
+timer = document.querySelector('.timer');
 
 start.addEventListener('click', startGame);
 playAgain.addEventListener('click', startGame)
@@ -20,7 +21,7 @@ let toBeChosen = [];
 let round_number = 1;
 let delayInMilliseconds = 500; //1 second
 let score = 0;
-let playerTurn = false;
+let lose = false;
 
 grid.setAttribute('style',
     `grid-template-columns: repeat(${GRID_SIZE}, ${TILE_SIZE}px);
@@ -53,12 +54,24 @@ function blinkEnd(){
     this.classList.remove('blink');
 }
 
+// Timer function
+// function timer(){
+//     var sec = 30;
+//     var timer = setInterval(function(){
+//         document.getElementById('safeTimerDisplay').innerHTML='00:'+sec;
+//         sec--;
+//         if (sec < 0) {
+//             clearInterval(timer);
+//         }
+//     }, 1000);
+// }
+
 // Start Game
 function startGame(){
     round_number = 1;
     toBeChosen = [];
     score = 0;
-    playerTurn = false;
+    lose = false;
 
     result.classList.add('hide');
     start.classList.add('hide');
@@ -66,6 +79,24 @@ function startGame(){
 
 
     displayPattern();
+
+    // Starts the timer for that round
+    function Timer(){
+        var sec = 0;
+        var min = 0;
+        var Sec = 0
+        var timerloop = setInterval(function(){
+            timer.innerHTML= `Time: ${min.toString().padStart(2, '0')}:${Sec.toString().padStart(2, '0')}`;
+            sec = sec + 0.1;
+            Sec = Math.floor(sec%60);
+            min = Math.floor(sec/60);
+            if (lose) {
+                resultText.textContent = `Score : ${Math.max(0,(round_number-1)*100 - (Sec*1 + min*60))}`
+                clearInterval(timerloop);
+            }
+        }, 100);
+    }
+    Timer();
 
 
     
@@ -75,6 +106,7 @@ function startGame(){
 function displayPattern(){
     toBeChosen = [];
     
+    // Disabling user tile press
     arrTile.forEach(tile => {
         tile.classList.add('disable');
         console.log(tile);
@@ -82,6 +114,7 @@ function displayPattern(){
     } )  
     startText.classList.add('hide');
     
+    // Making random tiles blink and adding them to toBeChosen
     for( let i = 0; i < round_number; i++){
         setTimeout(function() {   
             let item = arrTile[Math.floor(Math.random()*arrTile.length)];
@@ -90,12 +123,18 @@ function displayPattern(){
             blink(item);    
         }, delayInMilliseconds*(i+1));
     }
+
+    // Alloweing users to click on Tiles
     setTimeout(() => {   
         arrTile.forEach(tile => {
         tile.classList.remove('disable');
         startText.classList.remove('hide');
     } ) 
     }, delayInMilliseconds*round_number + 250);
+
+
+
+    
     
     
     
@@ -129,10 +168,10 @@ function userPress(){
 
 // Game over function - Displays Play Again
 function gameOver(){
+    lose = true;
     gameEndSound.play();
     toBeChosen = [];
     result.classList.remove('hide');
-    resultText.textContent = `Score : ${score}`;
 }
 
 createInitial();
